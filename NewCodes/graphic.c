5b6153bcac97ac0902fa2e640c37f76bc21785b1
Akshay-2007-1 <131676168+Akshay-2007-1@users.noreply.github.com>
2024-10-27 01:14:57 +0530

    Create graphic.c

---
+++ NewCodes/graphic.c
@@ -0,0 +1,68 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_timer.h>
+#include "graphic.h"
+
+int move()
+{
+	SDL_Init(SDL_INIT_VIDEO);
+	IMG_Init(IMG_INIT_PNG);
+	SDL_Window *win = SDL_CreateWindow("GAME",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,1000, 1000, 0);
+	SDL_Renderer* renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
+
+	SDL_Surface *bgSurface = IMG_Load("field.png");
+	SDL_Texture *bgTexture = SDL_CreateTextureFromSurface(renderer, bgSurface);
+
+	SDL_FreeSurface(bgSurface);
+	
+	SDL_Surface *playerSurface = IMG_Load("player.png");
+    SDL_Texture *playerTexture = SDL_CreateTextureFromSurface(renderer, playerSurface);
+    SDL_FreeSurface(playerSurface);
+
+    SDL_Rect playerRect = { 1000 / 2 - 25, 1000 / 2 - 25, 100, 100 };
+
+	SDL_Event event;
+
+	int go = 1;
+	while(go){
+		while(SDL_PollEvent(&event) != 0){
+			if(event.type == SDL_QUIT){
+				go = 0;
+				break;
+			}
+			if(event.type == SDL_KEYDOWN){
+				if(event.key.keysym.sym == SDLK_w){
+					playerRect.y -= 20;
+					key = 'W';
+					go = 0;
+				}else if(event.key.keysym.sym == SDLK_s){
+					playerRect.y += 20;
+					key = 'S';
+					go = 0;
+				}else if(event.key.keysym.sym == SDLK_d){
+					playerRect.x += 20;
+					key = 'D';
+					go = 0;
+				}else if(event.key.keysym.sym == SDLK_a){
+					playerRect.x -= 20;
+					key = 'A';
+					go = 0;
+				}
+			}
+		}
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_RenderClear(renderer);
+		SDL_RenderCopy(renderer, bgTexture, NULL, NULL);
+		
+		SDL_RenderCopy(renderer, playerTexture, NULL, &playerRect);
+        SDL_RenderPresent(renderer);
+		SDL_Delay(16);
+	}
+	
+	SDL_DestroyTexture(bgTexture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(win);
+    IMG_Quit();
+    SDL_Quit();
+	return 0;
+}
